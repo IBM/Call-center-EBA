ce39fe66264546a62a4cba76eb0436f6aa7a5b7c
Prarthana <prarmj10@in.ibm.com>
2020-02-14 18:58:45 +0530

    Update GetAccessKey.js

--- access_key/GetAccessKey.js
+++ access_key/GetAccessKey.js
@@ -1,17 +1,19 @@
 const fs = require('fs')
 const jwt = require('jsonwebtoken')
 
+
 const settings = {
-...     url: 'https://eba.ibm.com/',
-...     key: '<PATH TO private key Generated>',
-...     iss: 'https://idaas.iam.ibm.com',
-...     sub: '<EMAIL>',
-...     name: '<NAME>'
-... }
+    url: 'https://eba.ibm.com/',
+    key: 'PATH to the private key',
+    iss: 'https://idaas.iam.ibm.com',
+    sub: '<EMAIL>',
+    name: '<NAME>'
+}
 let claims = {
-...     iss: settings.iss,
-...     sub: settings.sub,
-...     name: settings.name
-... }
+    iss: settings.iss,
+    sub: settings.sub,
+    name: settings.name
+}
+
 let access_token = jwt.sign(claims, fs.readFileSync(settings.key), { algorithm: 'RS256' })
 console.log(access_token)
